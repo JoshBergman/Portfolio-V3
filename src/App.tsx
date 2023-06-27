42bcc3f630a01a90ce6f44b227c696b1dbb6dd87
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-06-27 11:00:25 -0500

    Mostly setup starting components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import "./App.css";
+import { addSiteView } from "./Private/AddView";
 
 function App() {
+  useEffect(() => {
+    addSiteView();
+  }, []);
+
   return <div className="page-control">App</div>;
 }
 
